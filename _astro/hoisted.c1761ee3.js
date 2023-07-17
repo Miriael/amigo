6e513a1fa385a9151f83792d679557023f8a7756
Miriael <ravafiel@protonmail.com>
2023-07-17 08:44:05 +0200

    a

--- _astro/hoisted.c1761ee3.js
+++ _astro/hoisted.c1761ee3.js
@@ -1,1 +1,1 @@
-import{S as e}from"./splide.esm.a7d191c3.js";import"./hoisted.06ca98c7.js";new e(".splide",{autoplay:!0,arrows:!1,interval:4e3,type:"loop",perPage:2,gap:"3em",lazyLoad:"nearby"}).mount();
+import{S as e}from"./splide.esm.a7d191c3.js";import"./hoisted.9b500507.js";new e(".splide",{autoplay:!0,arrows:!1,interval:4e3,type:"loop",perPage:2,gap:"3em",lazyLoad:"nearby"}).mount();
